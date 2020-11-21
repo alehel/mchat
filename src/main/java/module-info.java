ee34e4bfa9aad98d41fd8690e27a02831363de18
Aleksander Helgaker <aleksander.helgaker@gmail.com>
2020-11-21 14:20:34 +0100

    Remove module-info.java. Might reintroduce later.

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module io.aleksander {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens io.aleksander.mchat.controller to javafx.fxml;
-    exports io.aleksander.mchat.controller;
-}
