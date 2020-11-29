9dd975fb5b0a8f4fa87adc98c8cd475412b99c48
Aleksander Helgaker <aleksander.helgaker@gmail.com>
2020-11-29 13:43:28 +0100

    Add Jacoco to get test coverage in Sonarcloud, and ignore rule for unused import in Java Record class.

--- src/main/java/io/aleksander/mchat/model/Message.java
+++ src/main/java/io/aleksander/mchat/model/Message.java
@@ -2,5 +2,6 @@ package io.aleksander.mchat.model;
 
 import java.io.Serializable;
 
+@SuppressWarnings("squid:S1128")
 public record Message(String uuid, String sender, String content, String timestamp) implements Serializable {
 }
